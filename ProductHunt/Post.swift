f47ed3b916ada8e25950676c8c87246ad1cb749d
MondaleFelix <mondalefelix@yahoo.com>
2020-04-17 17:44:02 -0700

    created post model

---
+++ ProductHunt/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  ProductHunt
+//
+//  Created by Mondale on 4/17/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+    
+}
